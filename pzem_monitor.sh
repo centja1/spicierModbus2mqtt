391474a5a827ec42c50e4aa651f4bd70e4873833
Justin Centanni <justin.centanni@gmail.com>
2020-09-04 14:06:43 -0500

    Modified command line

--- pzem_monitor.sh
+++ pzem_monitor.sh
@@ -1,1 +1,1 @@
-python3 $1/modbus2mqtt.py --add-to-homeassistant  --config $1/centanni.csv --rtu $2 --mqtt-host mqtt.centanniventures.com --mqtt-topic DEV --rtu-baud 9600 --rtu-parity none --verbosity 4 --msg-format json_
+python3 $1/modbus2mqtt.py --config $1/centanni.csv --rtu $2 --mqtt-host mqtt.centanniventures.com --mqtt-topic DEV --rtu-baud 9600 --rtu-parity none --verbosity 4 --msg-format json
